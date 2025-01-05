dff311ef30837a2910f4403ea0f35fc5b52496fb
Breera Hanif <34021979+breera@users.noreply.github.com>
2025-01-05 22:23:30 +0500

    Added core module - Basic API client call structure - Add one reusable component

---
+++ core/src/main/java/com/breera/core/domain/DataError.kt
@@ -0,0 +1,13 @@
+package com.breera.core.domain
+
+sealed interface DataError: Error {
+    enum class Remote: DataError {
+        REQUEST_TIMEOUT,
+        TOO_MANY_REQUESTS,
+        NO_INTERNET,
+        SERVER,
+        SERIALIZATION,
+        NO_MATCH_FOUND,
+        UNKNOWN
+    }
+}
